e6ee2323ac1027676cc469111f72bf7dc12ee5fc
Ove Doll <tarpier@users.noreply.github.com>
2018-01-20 22:03:16 +0100

    fixed authentication

--- index.js
+++ index.js
@@ -72,48 +72,6 @@ apiRoutes.get("/users", function(req, res) {
   });
 });
 
-apiRoutes.post("/authenticate", function(req, res) {
-  // find the user
-  User.findOne(
-    {
-      name: req.body.name
-    },
-    function(err, user) {
-      if (err) throw err;
-
-      if (!user) {
-        res.json({
-          success: false,
-          message: "Authentication failed. User not found."
-        });
-      } else if (user) {
-        // check if password matches
-        if (user.password != req.body.password) {
-          res.json({
-            success: false,
-            message: "Authentication failed. Wrong password."
-          });
-        } else {
-          // if user is found and password is right
-          // create a token with only our given payload
-          // we don't want to pass in the entire user since that has the password
-          const payload = {
-            admin: user.admin
-          };
-          var token = jwt.sign(payload, app.get("superSecret"));
-
-          // return the information including token as JSON
-          res.json({
-            success: true,
-            message: "Enjoy your token!",
-            token: token
-          });
-        }
-      }
-    }
-  );
-});
-
 apiRoutes.get("/getinfo/:username", function(req, res) {
   const userName = req.params.username;
   const options = {
@@ -214,6 +172,48 @@ app.use("/api", apiRoutes);
 // UNPROTECTED PUBLIC ROUTES ----------------
 app.get("/", function(req, res) {
   res.json({ message: "nothing to see here" });
+});
+
+app.post("/authenticate", function(req, res) {
+  // find the user
+  User.findOne(
+    {
+      name: req.body.name
+    },
+    function(err, user) {
+      if (err) throw err;
+
+      if (!user) {
+        res.json({
+          success: false,
+          message: "Authentication failed. User not found."
+        });
+      } else if (user) {
+        // check if password matches
+        if (user.password != req.body.password) {
+          res.json({
+            success: false,
+            message: "Authentication failed. Wrong password."
+          });
+        } else {
+          // if user is found and password is right
+          // create a token with only our given payload
+          // we don't want to pass in the entire user since that has the password
+          const payload = {
+            admin: user.admin
+          };
+          var token = jwt.sign(payload, app.get("superSecret"));
+
+          // return the information including token as JSON
+          res.json({
+            success: true,
+            message: "Enjoy your token!",
+            token: token
+          });
+        }
+      }
+    }
+  );
 });
 
 /* app.get("/setup", function(req, res) {
